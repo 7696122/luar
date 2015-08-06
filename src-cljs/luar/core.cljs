b03925cb041987a071dad952e8a7b6744fb2afdb
7696122 <7696122@gmail.com>
2015-08-07 00:52:33 +0900

    set up cljs

---
+++ src-cljs/luar/core.cljs
@@ -0,0 +1,5 @@
+(ns luar.core)
+
+(enable-console-print)
+
+(println "Hello world!")
---
+++ src/luar/server.clj
@@ -0,0 +1,35 @@
+(ns luar.server
+  (:use [org.httpkit.server :only [run-server]])
+  (:require [ring.middleware.reload :as reload]
+            [ring.util.response :as response]
+            ;; [ring.middleware.refresh :as refresh]
+
+            [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]))
+
+;; (def server
+;;   (refresh/wrap-refresh all-routes))
+
+(defroutes all-routes
+  (GET "/" [] 
+       (response/file-response "index.html" {:root "resources/public"}))
+  (route/not-found "<h1>Page not found.</h1>"))
+
+(defn in-dev? [& args] true)
+
+(defn -main [& args]
+  (let [handler 
+        (if (in-dev? args)
+          (reload/wrap-reload #'all-routes)
+          all-routes)]
+    
+    (run-server handler {:port 8080})))
+
+
+
+
+
+
+
+
+
