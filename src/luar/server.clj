d36f2adb100edaf352e39823480e2c83c43ad3f9
7696122 <7696122@gmail.com>
2015-08-08 23:51:49 +0900

    set resource for compojure

--- src/luar/server.clj
+++ src/luar/server.clj
@@ -1,34 +1,36 @@
 (ns luar.server
   (:use [org.httpkit.server :only [run-server]])
   (:require [ring.middleware.reload :as reload]
-            [ring.util.response :as response]
             [ring.middleware.refresh :as refresh]
+            [ring.util.response :as response]
 
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]))
 
 (defroutes all-routes
   (GET "/" [] (response/file-response "index.html" {:root "resources/public"}))
-  (route/not-found "<h1>Page not found.</h1>"))
-
-;; (def server
-;;   (refresh/wrap-refresh all-routes))
+  (route/resources "/")
+  (route/not-found "<h1>Page not found!</h1>"))
 
 (defn in-dev? [& args] true)
 
+(def app 
+  ;; (wrap-file all-routes "resources/public")
+  ;; (refresh/wrap-refresh all-routes)
+  (if (in-dev?)
+    (refresh/wrap-refresh
+     (reload/wrap-reload #'all-routes))
+    all-routes))
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn start-server [& port]
+  (reset! server (run-server #'app {:port 8080})))
+
 (defn -main [& args]
-  (let [handler 
-        (if (in-dev? args)
-          (reload/wrap-reload #'all-routes)
-          all-routes)]
-    
-    (run-server handler {:port 8080})))
-
-
-
-
-
-
-
-
-
+  (start-server))
